9cd831ed6eb91eaca4a697c1a7304ad7c3189a30
Narothe <110347585+Narothe@users.noreply.github.com>
2023-04-16 22:25:56 +0200

    adding SettingsActivity

---
+++ app/src/main/java/com/example/flextube/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.flextube.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flextube.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
