5cfc027d80bdedbbdca8250edcf1f19edd30c7b5
Paweł <pawel.kam4@gmail.com>
2023-04-16 21:55:08 +0200

    devleop

--- app/src/main/java/com/example/flextube/MainActivity.kt
+++ app/src/main/java/com/example/flextube/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         // tu będzie coś
+        // albo nie
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
