1b72a64d71593fb4ddc79576a539209a6f07fbd3
Paweł <pawel.kam4@gmail.com>
2023-04-16 21:29:53 +0200

    namespace

--- app/src/main/java/com/example/flextube/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/flextube/ui/library/LibraryFragment.kt
@@ -1,4 +1,4 @@
-package com.example.flextube.ui.notifications
+package com.example.flextube.ui.library
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.flextube.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class LibraryFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -23,7 +23,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(LibraryViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
