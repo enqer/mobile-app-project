762e7d808165931e3c8ae0edf1a81bfc82efa7ed
Narothe <110347585+Narothe@users.noreply.github.com>
2023-04-23 01:22:34 +0200

    adding some change language implementation
    still in progress

--- app/src/main/java/com/example/flextube/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/flextube/settings/SettingsActivity.kt
@@ -1,58 +1,130 @@
 package com.example.flextube.settings
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Im
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.util.AttributeSet
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.AppCompatTextView
 import com.example.flextube.R
+import java.util.Locale
+
+
 
 class SettingsActivity : AppCompatActivity() {
 
     lateinit var switch1: Switch
+    //lateinit var languageTv: TextView
+    private lateinit var sharedPreferences: SharedPreferences
+    private val languageChangeListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            if (key == "language") {
+                val selectedLanguage = sharedPreferences.getString("language", "")
+                updateLanguageTextView(selectedLanguage) // Aktualizujemy tekst w TextView
+                updateLocale(selectedLanguage) // Aktualizujemy język na poziomie systemowym
+            }
+        }
 
-    fun closeClick(closeButtonIcon: ImageView){
-        closeButtonIcon.setOnClickListener {
+    private fun updateLanguageTextView(language: String?) {
+        val languageTv = findViewById<TextView>(R.id.language_TV)
+        val languageKey = if (language == "Polski") "language_pl" else "language_en"
+        val languageText = resources.getString(resources.getIdentifier(languageKey, "string", packageName), language)
+        languageTv.text = languageText
+    }
+
+    private fun updateLocale(language: String?) {
+        val locale = if (language == "Polski") {
+            Locale("pl")
+        } else {
+            Locale("en")
+        }
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Usuwamy obserwatora przy zamykaniu aktywności
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener { sharedPreferences, key -> }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val closeButtonIcon = findViewById<ImageView>(R.id.close_button)
+        val languageTv = findViewById<TextView>(R.id.language_TV)
+
+        switch1 = findViewById(R.id.switch1)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        // Odczytywanie zapisanego języka
+        val savedLanguage = sharedPreferences.getString("language", "")
+        updateLanguageTextView(savedLanguage)
+
+        // Initialize LinearLayout and add support for the click event
+        val linearLayout2 = findViewById<LinearLayout>(R.id.linearLayout2) // language
+        linearLayout2.setOnClickListener {
+            val languages = arrayOf("English", "Polski") // Przykładowe dostępne języki
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle("Choose Language")
+                .setItems(languages) { dialog, which ->
+                    val selectedLanguage = languages[which]
+                    // Tutaj wykonaj akcję związana z wyborem języka
+                    updateLanguageTextView(selectedLanguage) // Aktualizujemy tekst w TextView
+                    updateLocale(selectedLanguage) // Aktualizujemy język na poziomie systemowym
+                    Toast.makeText(this, "Selected Language: $selectedLanguage", Toast.LENGTH_SHORT).show()
+
+                    // Zapisywanie wybranego języka
+                    val editor = sharedPreferences.edit()
+                    editor.putString("language", selectedLanguage)
+                    editor.apply()
+                    dialog.dismiss() // Zamknięcie okna dialogowego po wyborze języka
+                }
+                .setNegativeButton("Cancel") { dialog, _ ->
+                    dialog.dismiss() // Zamknięcie okna dialogowego po kliknięciu przycisku "Cancel"
+                }
+            alertDialogBuilder.create().show()
+        }
+
+        // Dodajemy obserwatora na zmiany wartości w SharedPreferences
+        sharedPreferences.registerOnSharedPreferenceChangeListener(languageChangeListener)
+
+        closeButtonIcon.setOnClickListener { // close button
             // Here put the code to be executed when you click item
             Toast.makeText(this, "Close", Toast.LENGTH_SHORT).show()
         }
-    }
 
-    fun languageClick(){
-        // Initialize LinearLayout and add support for the click event
-        val linearLayout = findViewById<LinearLayout>(R.id.linearLayout2)
-        linearLayout.setOnClickListener {
-            // Here put the code to be executed when you click LinearLayout
-            Toast.makeText(this, "Language", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    fun settingsClick(){
-        val linearLayout = findViewById<LinearLayout>(R.id.linearLayout3)
-        linearLayout.setOnClickListener {
+        val linearLayout3 = findViewById<LinearLayout>(R.id.linearLayout3) // settings
+        linearLayout3.setOnClickListener {
             Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
         }
-    }
 
-    fun helpClick(){
-        val linearLayout = findViewById<LinearLayout>(R.id.linearLayout5)
-        linearLayout.setOnClickListener {
+        val linearLayout5 = findViewById<LinearLayout>(R.id.linearLayout5) // help
+        linearLayout5.setOnClickListener {
             Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show()
         }
-    }
 
-    fun howItWorksClick(){
-        val linearLayout = findViewById<LinearLayout>(R.id.linearLayout6)
-        linearLayout.setOnClickListener {
+        val linearLayout6 = findViewById<LinearLayout>(R.id.linearLayout6) // how it works
+        linearLayout6.setOnClickListener {
             Toast.makeText(this, "How it works?", Toast.LENGTH_SHORT).show()
         }
-    }
 
-    fun darkModeSwitch(){
         // Switch state change support
         switch1.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -63,26 +135,6 @@ class SettingsActivity : AppCompatActivity() {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-
-        val closeButtonIcon = findViewById<ImageView>(R.id.close_button)
-        switch1 = findViewById(R.id.switch1)
-
-        closeClick(closeButtonIcon)
-        languageClick()
-        settingsClick()
-        helpClick()
-        howItWorksClick()
-
-        darkModeSwitch()
-        // notka: te funkcje jebnij to z powrotem tutaj
-        // notka: dodać to gówno na więcej theme
-        // notka: dodać inne kolory na tło całej aplikacji
-
 
     }
 }
